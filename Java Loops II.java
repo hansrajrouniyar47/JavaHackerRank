31085fd938e7036cdb6c266a5942c14c5040e91a
hansrajrouniyar47 <hansrajrouniyar47@gmail.com>
2021-10-19 15:48:18 +0545

    committing

---
+++ Java Loops II.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int temp;
+            int last = a + (int)(Math.pow(2,0) * b);
+           
+            for(int j = 1; j <= n; j++){
+                temp =  last + (int)(Math.pow(2,j) * b);
+                System.out.print(last+" ");
+                last = temp;
+            }
+            System.out.println();
+        }
+        
+        in.close();
+    }
+}
